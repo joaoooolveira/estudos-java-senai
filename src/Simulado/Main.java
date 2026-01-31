9d08d004e0edda3745efa3e11e4c5173372fec6c
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-30 22:52:26 -0300

    exercise - create a sales system for commerce

---
+++ src/Simulado/Main.java
@@ -0,0 +1,33 @@
+package Simulado;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vendas do dia\nAssim que encerrar digite 0 no valor e na forma de pagamento.");
+
+        double venda = 0;
+
+        Venda vendas = new Venda(venda);
+
+        while (true){
+            System.out.println();
+            System.out.print("Digite o valor da venda: ");
+            venda = sc.nextInt();
+
+            vendas.formaPagamento();
+            int escolha = sc.nextInt();
+
+            if (escolha == 0){
+                System.out.println("Finalizando programa...");
+                break;
+            }
+
+            vendas.escolhaDaFormaDePagamento(escolha, venda);
+        }
+
+        vendas.emitirVenda();
+    }
+}
---
+++ src/Simulado/Venda.java
@@ -0,0 +1,98 @@
+package Simulado;
+
+public class Venda {
+    private double valorVenda;
+    private int formaDePagamento;
+    private int totalDeVendas = 0;
+    private double valorTotalDasVendas = 0;
+    private double somaDinheiro = 0;
+    private double somaCredito = 0;
+    private double somaDebito = 0;
+    private double somaPix = 0;
+
+    public Venda(double valorVenda){
+        if(valorVenda < 0){
+            throw new IllegalArgumentException("Digite um valor acima de 0.");
+        }
+        this.valorVenda = valorVenda;
+    }
+
+    public void escolhaDaFormaDePagamento(int escolha, double valorVenda){
+        switch(escolha){
+            case 1:
+                somaDinheiro += valorVenda;
+                System.out.println(String.format("Valor de R$%.2f computado em Dinheiro.", valorVenda));
+                valorTotalDasVendas += valorVenda;
+                totalDeVendas++;
+                break;
+            case 2:
+                somaCredito += valorVenda;
+                System.out.println(String.format("Valor de R$%.2f computado em Crédito.", valorVenda));
+                valorTotalDasVendas += valorVenda;
+                totalDeVendas++;
+                break;
+            case 3:
+                somaDebito += valorVenda;
+                System.out.println(String.format("Valor de R$%.2f computado em Débito", valorVenda));
+                valorTotalDasVendas += valorVenda;
+                totalDeVendas++;
+                break;
+            case 4:
+                somaPix += valorVenda;
+                System.out.println(String.format("Valor de R$%.2f computado em Pix", valorVenda));
+                valorTotalDasVendas += valorVenda;
+                totalDeVendas++;
+                break;
+        }
+    }
+
+    public void formaPagamento(){
+        System.out.println("""
+                1- Dinheiro
+                2- Cartão de Crédito
+                3- Cartão de Débito
+                4- Pix
+                0- Encerrar""");
+    }
+
+    public void emitirVenda(){
+        System.out.println("""
+                Veja suas estatísticas do dia:
+                
+                ***********************************
+                
+                Total de vendas: %d
+                Valor total arrecadado: R$%.2f
+                Total por forma de pagamento:
+                Dinheiro: R$%.2f
+                Cartão de crédito: R$%.2f
+                Cartão de débito: R$%.2f
+                Pix: R$%.2f
+                
+                ***********************************""".formatted(getTotalDeVendas(), getValorTotalDasVendas(), getSomaDinheiro(), getSomaCredito(), getSomaDebito(), getSomaPix()));
+    }
+
+    public int getTotalDeVendas() {
+        return totalDeVendas;
+    }
+
+    public double getValorTotalDasVendas() {
+        return valorTotalDasVendas;
+    }
+
+    public double getSomaDinheiro() {
+        return somaDinheiro;
+    }
+
+    public double getSomaCredito() {
+        return somaCredito;
+    }
+
+    public double getSomaDebito() {
+        return somaDebito;
+    }
+
+    public double getSomaPix() {
+        return somaPix;
+    }
+}
