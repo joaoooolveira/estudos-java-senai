1906db0a1c92b07aa1e4e4cf1e505cd247161220
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-02-09 16:32:45 -0300

    create class Produto

--- src/revisaoPOO/Main.java
+++ src/revisaoPOO/Main.java
@@ -3,9 +3,9 @@ package revisaoPOO;
 public class Main {
     public static void main(String[] args){
         Aluno aluno1 = new Aluno("Joao", 18, "1", "Desenvolvimento de Sistemas");
-        Professor professor1 = new Professor("Iago", 28, "Desenvolvimento de Sistemas", 3000);
+        Professor iago = new Professor("Iago", 28, "Desenvolvimento de Sistemas", 3000);
 
         System.out.println(aluno1.exibirDados() + "\n");
-        System.out.println(professor1.exibirDados());
+        System.out.println(iago.exibirDados());
     }
 }
--- src/revisaoPOO/Pessoa.java
+++ src/revisaoPOO/Pessoa.java
@@ -1,6 +1,6 @@
 package revisaoPOO;
 
-public class Pessoa {
+public abstract class Pessoa {
     private String nome;
     private int idade;
 
